759130d47d4ab4e85fb1587125e0049aaa162c3a
Johannes Eichner <johannes.eichner@gmail.com>
2012-05-11 14:45:15 +0000

    Added animated char to stand-alone version.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/tfpredict@44 71221333-9ef9-431f-bb7f-117e0a61b720

---
+++ src/io/AnimatedChar.java
@@ -0,0 +1,44 @@
+package io;
+
+import java.io.PrintStream;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class AnimatedChar {
+    private final static String anim= "|/-\\";
+    
+    int callNumber = 0;
+    Timer t = null;
+    
+    PrintStream out = System.out;
+    
+    public void setOutputStream(PrintStream out) {
+    	this.out = out;
+    }
+    
+    public void showAnimatedChar() {
+    	
+    	t = new Timer();
+    	t.schedule(new TimerTask() {
+			@Override
+			public void run() {
+		    	callNumber = (callNumber+1)% anim.length();
+		    	char c = anim.charAt(callNumber);
+		    	out.print('\r');
+		    	out.print(c);
+			}
+		}, 200, 200);
+    }
+    
+    public void hideAnimatedChar() {
+    	if (t!=null) {
+    		t.cancel();
+    		out.print('\r');
+    	}
+    }
+    
+    
+    
+    
+    
+}
