0290f2d87d7c6edceaee0d7ac2c6fc31372af5f9
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2013-02-27 09:51:19 +0000

    Added license headers to all java files and created a licenses file including an Authors.txt and Copying.txt file.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/tfpredict@80 71221333-9ef9-431f-bb7f-117e0a61b720

--- src/io/FTPsupport.java
+++ src/io/FTPsupport.java
@@ -1,10 +1,27 @@
-/*
- * ===============================================
- * (C) Florian Topf, University of Tuebingen, 2010
- * ===============================================
+/*  
+ * $Id$
+ * $URL$
+ * This file is part of the program TFpredict. TFpredict performs the
+ * identification and structural characterization of transcription factors.
+ *  
+ * Copyright (C) 2010-2013 Center for Bioinformatics Tuebingen (ZBIT),
+ * University of Tuebingen by Johannes Eichner, Florian Topf, Andreas Draeger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package io;
+
 import it.sauronsoftware.ftp4j.FTPAbortedException;
 import it.sauronsoftware.ftp4j.FTPClient;
 import it.sauronsoftware.ftp4j.FTPDataTransferException;
@@ -15,8 +32,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.SocketException;
 
-// This the FTP client library from www.sauronsoftware.it/projects/ftp4j/
-
+/** 
+ * This the FTP client library from www.sauronsoftware.it/projects/ftp4j/
+ * @author Florian Topf
+ * @version $Rev$
+ * @since 1.0
+ */
 public class FTPsupport {
 
 	private String domain;
